eb7c524f9c676bc58990dae66d91332c51248f68
kazenetu <rakudafactory@gmail.com>
2018-01-04 15:11:30 +0900

    トークン発行機能を追加

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -8,39 +8,60 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-
-            // 静的ファイルのデフォルト設定を有効にする
-            app.UseDefaultFiles();
-
-            // 静的ファイルを使用する
-            app.UseStaticFiles();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+
+      // トークン設定
+      services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IAntiforgery antiforgery)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvc();
+
+      // ルートアクセス時にトークン発行
+      app.Use(next => context =>
+      {
+        if (
+            string.Equals(context.Request.Path.Value, "/", StringComparison.OrdinalIgnoreCase) ||
+            string.Equals(context.Request.Path.Value, "/index.html", StringComparison.OrdinalIgnoreCase))
+        {
+          // We can send the request token as a JavaScript-readable cookie, and Angular will use it by default.
+          var tokens = antiforgery.GetAndStoreTokens(context);
+          context.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
+                    new CookieOptions() { HttpOnly = false });
+        }
+
+        return next(context);
+      });
+
+      // 静的ファイルのデフォルト設定を有効にする
+      app.UseDefaultFiles();
+
+      // 静的ファイルを使用する
+      app.UseStaticFiles();
+    }
+  }
 }
