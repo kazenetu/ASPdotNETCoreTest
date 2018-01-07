3c107309dcf1a3f12df0bce2dd0165d0dce0b346
kazenetu <rakudafactory@gmail.com>
2018-01-07 23:51:19 +0900

    プロパティのスコープを変更

--- Domain/Service/User/UserSeachCondition.cs
+++ Domain/Service/User/UserSeachCondition.cs
@@ -1,7 +1,7 @@
 namespace Domain.Service.User
 {
   /// <summary>
-  /// 検索条件
+  /// 検索条件クラス
   /// </summary>
   public class UserSeachCondition
   {
@@ -10,24 +10,24 @@ namespace Domain.Service.User
     /// 検索条件：ユーザーID
     /// </summary>
     /// <returns>ユーザーID(未選択時:string.Empty)</returns>
-    public string SearchUserId{private set;get;} = string.Empty;
+    public string SearchUserId{set;get;} = string.Empty;
 
     /// <summary>
     /// 表示対象のページインデックス
     /// </summary>
     /// <returns>ページインデックス</returns>
-    public int PageIndex{private set;get;} = 0;
+    public int PageIndex{set;get;} = 0;
     
     /// <summary>
     /// ソートキー
     /// </summary>
     /// <returns>ソートキー</returns>
-    public string SortKey{private set;get;} = string.Empty;
+    public string SortKey{set;get;} = string.Empty;
 
     /// <summary>
     /// ソートタイプ
     /// </summary>
     /// <returns>ソートタイプ(昇順・降順)</returns>
-    public string SortType{private set;get;} = string.Empty;
+    public string SortType{set;get;} = string.Empty;
   }
 }
