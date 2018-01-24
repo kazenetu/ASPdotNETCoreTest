f44db313bf0e8794690b6309818e4ab0113ab2d1
kazenetu <rakudafactory@gmail.com>
2018-01-24 21:25:10 +0900

    出力対象の物理カラム名と論理カラム名のリストを渡せるように機能拡張

--- Domain/Model/ModelBase.cs
+++ Domain/Model/ModelBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Domain.Model
 {
@@ -9,33 +10,71 @@ namespace Domain.Model
   public abstract class ModelBase
   {
     /// <summary>
-    /// CSV出力
+    /// CSV出力(ヘッダーなし・カラム設定なし)
     /// </summary>
     /// <returns>カンマ区切りの文字列</returns>
     public string GetCSV()
     {
-      return GetCSV(false);
+      return GetCSV(false, new Dictionary<string, string>());
+    }
+
+    /// <summary>
+    /// CSV出力(カラム設定なし)
+    /// </summary>
+    /// <param name="header">ヘッダー付きか否か</param>
+    /// <returns>カンマ区切りの文字列</returns>
+    public string GetCSV(bool header)
+    {
+      return GetCSV(header, new Dictionary<string, string>());
+    }
+
+    /// <summary>
+    /// CSV出力(ヘッダーなし)
+    /// </summary>
+    /// <returns>カンマ区切りの文字列</returns>
+    /// <param name="columnList">物理カラム名と論理カラム名のリスト</param>
+    public string GetCSV(Dictionary<string, string> columnList)
+    {
+      return GetCSV(false, columnList);
     }
 
     /// <summary>
     /// CSV出力
     /// </summary>
     /// <param name="header">ヘッダー付きか否か</param>
+    /// <param name="columnList">物理カラム名と論理カラム名のリスト</param>
     /// <returns>カンマ区切りの文字列</returns>
-    public string GetCSV(bool header)
+    public string GetCSV(bool header, Dictionary<string, string> columnList)
     {
       var properties = GetType().GetProperties();
 
       var title = new List<string>();
       var data = new List<string>();
 
-      foreach(var property in properties)
+      // カラム名が未定義の場合は物理名を設定する
+      if (!columnList.Any())
       {
+        foreach(var property in properties)
+        {
+          columnList.Add(property.Name, property.Name);
+        }
+      }
+
+      foreach(var column in columnList)
+      {
+        // 対象のプロパティを取得
+        var targetPropertys = properties.Where((item)=>item.Name == column.Key);
+        if(!targetPropertys.Any()){
+          continue;
+        }
+
         if(header)
         {
-          title.Add(property.Name);
+          // ヘッダー作成
+          title.Add(column.Value);
         }
-        data.Add(property.GetValue(this) as string);
+        // データ作衛
+        data.Add(targetPropertys.First().GetValue(this) as string);
       }
 
       var result = string.Join(",",title);
