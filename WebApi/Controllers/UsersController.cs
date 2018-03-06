b985ed33417c5130f3a07373c480cad49572cf94
kazenetu <rakudafactory@gmail.com>
2018-03-06 22:26:09 +0900

    移植済のログイン・ログアウトメソッドを削除

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -36,7 +36,6 @@ namespace WebApi.Controllers
     #endregion
 
     #region プライベート定数フィールド
-    private static string ErrorLoginNG = "ログイン失敗";
     private static string ErrorPasswordNG = "パスワード失敗";
     private static string SearchResultZero = "検索結果ゼロ件";
     private static string ErrorNotFound = "データが見つかりません";
@@ -60,105 +59,6 @@ namespace WebApi.Controllers
     #endregion
 
     #region パブリックメソッド
-
-    #region ログイン・ログアウト
-
-    /// <summary>
-    /// ログイン
-    /// </summary>
-    /// <param name="param">入力情報</param>
-    /// <returns>結果(json)</returns>
-    /// <remarks>POST api/user/login</remarks>
-    [HttpPost("login")]
-    [AutoValidateAntiforgeryToken]
-    public IActionResult Login([FromBody]Dictionary<string, object> param)
-    {
-      var paramNameUserId = "id";
-      var paramNamePassword = "password";
-
-      // 入力チェック
-      if (!param.ContainsKey(paramNameUserId))
-      {
-        logger.LogError("Pram[{0}]が未設定", paramNameUserId);
-        return BadRequest();
-      }
-      if (!param.ContainsKey(paramNamePassword))
-      {
-        logger.LogError("Pram[{0}]が未設定", paramNamePassword);
-        return BadRequest();
-      }
-
-      var userId = param[paramNameUserId].ToString();
-      var password = param[paramNamePassword].ToString();
-
-      var data = new Dictionary<string, object>();
-      var serviceResult = false;
-      try
-      {
-        var passwordHash = string.Empty;
-
-        var model = service.Find(userId);
-        if (model != null && model.EntryDate.HasValue)
-        {
-          // パスワードのハッシュ取得
-          passwordHash = HashUtility.Create(model.UserID,password,model.EntryDate.Value);
-        }
-
-        // ログインチェック
-        if (service.Login(userId, passwordHash) != null)
-        {
-          // セッション破棄
-          refreshSession();
-
-          // セッションキー設定
-          session.SetString(SessionKeyUserID, userId);
-
-          serviceResult = true;
-          data.Add("name", model.UserName);
-        }
-      }
-      catch (Exception ex)
-      {
-        logger.LogCritical("{0}", ex.Message);
-        return BadRequest();
-      }
-
-      var result = new Dictionary<string, object>();
-      if (serviceResult)
-      {
-        result.Add("result", "OK");
-      }
-      else
-      {
-        result.Add("result", "NG");
-        result.Add("errorMessage", ErrorLoginNG);
-      }
-      result.Add("responseData", data);
-
-      return Json(result);
-    }
-
-    /// <summary>
-    /// ログアウト
-    /// </summary>
-    /// <param name="param">入力情報</param>
-    /// <returns>結果(json)</returns>
-    /// <remarks>POST api/user/logout</remarks>
-    [HttpPost("logout")]
-    [AutoValidateAntiforgeryToken]
-    public IActionResult Logout([FromBody]Dictionary<string, object> param)
-    {
-
-      // セッション破棄
-      HttpContext.Response.Cookies.Delete(ControllerBase.SessionCookieName);
-      session.Clear();
-
-      var result = new Dictionary<string, object>();
-      result.Add("result", "OK");
-
-      return Json(result);
-    }
-    #endregion
 
     #region 検索系
 
