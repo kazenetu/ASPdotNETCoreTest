ca0c6c5e38e3d4d8326e59c7fb1c70899f321697
kazenetu <rakudafactory@gmail.com>
2018-03-30 22:16:01 +0900

    API呼び出し処理の変更

--- WebApi/wwwroot/assets/controller/loginController.js
+++ WebApi/wwwroot/assets/controller/loginController.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    front.controller.LoginController = function LoginController($location, webApiService, userService) {
+    front.controller.LoginController = function LoginController($location, loginAPI, userService) {
         front.common.utils.extendController(this, front.common.controller.PageBase);
         this.setTitle('ログイン');
 
@@ -20,19 +20,22 @@
                 return;
             }
 
-            webApiService.post('api/account/login', {
-                id: ctrl.userId,
-                password: ctrl.password
-            }, function (response) {
-                if (response.result !== "OK") {
-                    ctrl.showError(response.errorMessage);
-                } else {
-                    ctrl.hideError();
-                    userService.setId(ctrl.userId);
-                    userService.setName(response.responseData.name);
-                    $location.path('/main');
+            loginAPI.login(
+                {
+                    id: ctrl.userId,
+                    password: ctrl.password
+                }, 
+                function (response) {
+                    if (response.result !== "OK") {
+                        ctrl.showError(response.errorMessage);
+                    } else {
+                        ctrl.hideError();
+                        userService.setId(ctrl.userId);
+                        userService.setName(response.responseData.name);
+                        $location.path('/main');
+                    }
                 }
-            });
+            );
         }
 
         /**
@@ -62,15 +65,18 @@
          * ページ初期化処理
          */
         ctrl.init = function () {
-            webApiService.post('api/account/logout', {
-                id: ctrl.userId,
-                password: ctrl.password
-            }, function (response) {
-            });
+            loginAPI.init(
+                {
+                    id: ctrl.userId,
+                    password: ctrl.password
+                }, 
+                function (response) {
+                }
+            );
         }
 
     }
 
     // コントローラー定義
-    angular.module('App').controller('loginController', ['$location', 'webApiService', 'userService', front.controller.LoginController]);
+    angular.module('App').controller('loginController', ['$location', 'loginAPI', 'userService', front.controller.LoginController]);
 }());
