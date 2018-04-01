f60c0068fdc464e249bc1724931cb3f1332714d2
kazenetu <rakudafactory@gmail.com>
2018-04-01 16:34:51 +0900

    ビジネスロジックとコントローラーを分ける試作を実施

--- WebApi/wwwroot/assets/controller/loginController.js
+++ WebApi/wwwroot/assets/controller/loginController.js
@@ -1,7 +1,75 @@
 (function () {
     'use strict';
 
-    front.controller.LoginController = function LoginController($location, loginAPI, userService) {
+    /**
+     * ログイン ビジネスロジック
+     * @param {Object} $q
+     * @param {Object} $location
+     * @param {LoginAPI} loginAPI 
+     * @param {UserService} userService 
+     */
+    function LoginLogic($q, $location, loginAPI, userService) {
+
+        /**
+         * ログイン処理
+         * @param {Array} params - 入力データ
+         * @param {Object} result - 結果データ
+         */
+        function login(params, result) {
+            var d = $q.defer();
+
+            // 発火
+            setTimeout(function () {
+                d.resolve();
+            }, 0);
+
+            return d.promise
+                .then(function (reject, resolve) {
+                    var deferrred = $q.defer();
+
+                    var result = {
+                        result: false,
+                        message: ''
+                    };
+
+                    loginAPI.login(params,
+                        function (response) {
+                            if (response.result !== "OK") {
+                                result.message = response.errorMessage;
+
+                                resolve = result;
+                                reject = result;
+
+                                return deferrred.resolve(result);
+                            }
+                            else {
+                                userService.setId(params.id);
+                                userService.setName(response.responseData.name);
+                                result.result = true;
+
+                                resolve = result;
+
+                                return deferrred.resolve(result);
+                            }
+                        });
+
+                    return deferrred.promise;
+                });
+        };
+
+        return {
+            login : login
+        }
+    }
+
+    // ロジックサービス定義
+    angular.module('App').factory('loginLogic', ['$q', '$location', 'loginAPI', 'userService', LoginLogic]);
+}());
+
+(function () {
+    'use strict';
+
+    front.controller.LoginController = function LoginController($q, $location, loginLogic, loginAPI, userService) {
         front.common.utils.extendController(this, front.common.controller.PageBase);
         this.setTitle('ログイン');
 
@@ -20,22 +88,21 @@
                 return;
             }
 
-            loginAPI.login(
-                {
-                    id: ctrl.userId,
-                    password: ctrl.password
-                }, 
-                function (response) {
-                    if (response.result !== "OK") {
-                        ctrl.showError(response.errorMessage);
-                    } else {
-                        ctrl.hideError();
-                        userService.setId(ctrl.userId);
-                        userService.setName(response.responseData.name);
-                        $location.path('/main');
-                    }
+            var params = {
+                id: ctrl.userId,
+                password: ctrl.password
+            };
+            var d = loginLogic.login(params);
+
+            d.then(function (result) {
+                if (result.result) {
+                    ctrl.hideError();
+                    $location.path('/main');
                 }
-            );
+                else {
+                    ctrl.showError(result.message);
+                }
+            });
         }
 
         /**
@@ -69,7 +136,7 @@
                 {
                     id: ctrl.userId,
                     password: ctrl.password
-                }, 
+                },
                 function (response) {
                 }
             );
@@ -78,5 +145,5 @@
     }
 
     // コントローラー定義
-    angular.module('App').controller('loginController', ['$location', 'loginAPI', 'userService', front.controller.LoginController]);
+    angular.module('App').controller('loginController', ['$q', '$location', 'loginLogic', 'loginAPI', 'userService', front.controller.LoginController]);
 }());
